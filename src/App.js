c3ba53e488b510f5e93d02c3661585a179b31c50
mayankpcp7 <jariwalmayank123@gmail.com>
2023-06-28 15:14:49 +0530

    push code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./root.css";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import Footercard from "./components/Footercard";
 function App() {
   return (
     <>
       {/* <Hero /> */}
       <Footer />
+      <Footercard />
     </>
   );
 }
