1bae663e1efe7186a37a5fe9d642b3066c9d8ee0
mayankpcp7 <jariwalmayank123@gmail.com>
2023-06-28 10:06:45 +0530

    pull root bg-color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                       <div className="col-lg-5">
                           <div className="d-flex justify-content-start align-items-center">
                               <div className="green_line"></div> 
-                              <div><h5 className="">Be</h5></div>
+                              <div><h5 className="ff_inter fw-light color_white">Be</h5></div>
                           </div>
                       </div>
                       <div className="col-lg-7">
